0bb9e9eb8729896531956067b5546389d69e0a0d
danielntll <116676924+danielntll@users.noreply.github.com>
2023-07-25 10:55:40 +0200

    Fix: #17
    Global.scss error fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    additionalData: `@import "./styles/globals.scss" `,
   },
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import styles from './Home.module.scss';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
@@ -14,6 +12,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        <div className={styles.HomePage}>
+          <h1 className={styles.title}>Ciao homepage</h1>
+        </div>
+      </main>
     </>
   )
 }
