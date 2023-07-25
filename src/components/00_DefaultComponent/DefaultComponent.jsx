4abb986626f2574ac205bbe0515e27755a89d1f9
danielntll <116676924+danielntll@users.noreply.github.com>
2023-07-25 17:19:15 +0200

    Feature : Inizializzazione Default component e Page

---
+++ src/components/00_DefaultComponent/DefaultComponent.jsx
@@ -0,0 +1,15 @@
+import styles from './DefaultComponent.module.scss'
+
+const DefaultComponent = () => {
+  // VARIABLES ----------------
+  // CONDITIONS ---------------
+  // FUNCTIONS ----------------
+  // RETURN -------------------
+  return (
+    <div className={styles.DefaultComponent}>
+      default Component
+    </div>
+  );
+}
+
+export default DefaultComponent;
