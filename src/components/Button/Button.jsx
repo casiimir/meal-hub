2a01497cf48d43a652d04b203c0c479f601bbc51
MichelaCerma <michela.cerma02@gmail.com>
2023-07-26 11:53:13 +0200

    feat: branch button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import styles from './Button.module.scss'
+
+const Button = () => {
+  // VARIABLES ----------------
+  // CONDITIONS ---------------
+  // FUNCTIONS ----------------
+  // RETURN -------------------
+  return (
+    <div className={styles.DefaultComponent}>
+        
+    </div>
+  );
+}
+
+export default Button;
