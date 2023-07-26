6c22822ac015b47205363ffa9fb14432247a3b09
MichelaCerma <michela.cerma02@gmail.com>
2023-07-26 13:52:02 +0200

    feat: button style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,16 +4,17 @@ const Button = ({
   isRound = false,
   text = "",
   icon = null,
-  isActive = false,
+  isNotActive = false,
   shadow = false,
   iconLeftSide = false,
+  border = false,
 }) => {
   // VARIABLES ----------------
   // CONDITIONS ---------------
   // FUNCTIONS ----------------
   // RETURN -------------------
   return (
-    <button className={`${styles.Button} ${isActive && styles.IsActive}`}>
+    <button className={`${styles.Button} ${isNotActive && styles.IsNotActive}`}>
       {icon && <span>{icon}</span>}
       {text && <span className={styles.ButtonText}>{text}</span>}
     </button>
