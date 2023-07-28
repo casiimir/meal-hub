b83a8c56fb39084587885e2cebd385a273d99e45
danielntll <dev@danielntll.it>
2023-07-28 15:19:18 +0200

    Feat: search sesults

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
-import styles from './SearchBar.module.scss';
-import Button from '../Button';
+import { useEffect, useState } from "react";
+import styles from "./SearchBar.module.scss";
+import Button from "../Button";
 import { LuSettings2, LuSearch } from "react-icons/lu";
-
+import { getData } from "@/utils/dbManager";
+import SearchedResult from "../searchedResult";
 
 const SearchBar = () => {
   // VARIABLES ----------------
@@ -12,62 +13,97 @@ const SearchBar = () => {
   const [searchString, setSearchString] = useState("");
 
   const [isSearching, setIsSearching] = useState(false);
+  const [dataSearched, setDataSearched] = useState();
+  const [dataToShow, setDataToShow] = useState();
   // FUNCTIONS ----------------
   useEffect(() => {
-    searchString.length !== 0 ? setCanSubmit(true) : setCanSubmit(false);
+    if (searchString.length !== 0) {
+      setCanSubmit(true);
+      handleSearchSelection(searchString);
+      dataSearched === null ? handleSearch(searchString) : null;
+    } else {
+      setCanSubmit(false);
+      setDataSearched(null);
+    }
   }, [searchString]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("OPEN FILTERED PAGE WITH THIS SEARCH INPUT : ", searchString);
-  }
+  };
+
+  const handleSearch = async (string) => {
+    setIsSearching(true);
+    const data = await getData.letter(string.charAt(0));
+    console.log(data);
+    setDataSearched(data.meals);
+    setDataToShow(data.meals);
+    setIsSearching(false);
+  };
+
+  const handleSearchSelection = (searchedText) => {
+    setDataToShow([]);
+    const query = searchedText.toLowerCase();
+    requestAnimationFrame(() => {
+      const auxArr = [];
+      dataSearched?.forEach((item) => {
+        const shouldShow = item.strMeal.toLowerCase().indexOf(query) > -1;
+        shouldShow ? auxArr.push(item) : null;
+      });
+      setDataToShow(auxArr);
+    });
+  };
   // RETURN -------------------
   return (
     <div className={styles.form}>
-      <form
-        onSubmit={(e) => handleSubmit(e)}
-        className={styles.container}
-      >
+      <form onSubmit={(e) => handleSubmit(e)} className={styles.container}>
         <div className={styles.searchIcon__container}>
-          {
-            !canSubmit ?
-              <div className={styles.searchIcon}>
-                <LuSearch size={24} />
-              </div>
-              :
-              <div className={styles.searchIcon}>
-                <LuSearch size={24} />
-              </div>
-          }
-
+          <div
+            className={
+              isOnFocus
+                ? `${styles.searchIcon__active}`
+                : `${styles.searchIcon}`
+            }
+          >
+            <LuSearch size={24} />
+          </div>
         </div>
         <input
           onFocus={() => setIsOnFocus(true)}
           onBlur={() => setIsOnFocus(false)}
           onChange={(e) => setSearchString(e.target.value)}
-          type='text'
-          placeholder='Search'
+          type="text"
+          placeholder="Search"
           className={styles.SearchBar}
           required
         />
         <Button size="lg" icon={(size) => <LuSettings2 size={size} />} />
       </form>
       <div
-        className={isOnFocus ?
-          `${styles.searchResults}  ${styles.isActive}`
-          :
-          `${styles.searchResults}  ${styles.notActive}`}
+        className={
+          canSubmit
+            ? `${styles.searchResults}  ${styles.isActive}`
+            : `${styles.searchResults}  ${styles.notActive}`
+        }
       >
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
-        <h1>ascdasdc</h1>
+        {isSearching ? (
+          <p>Searching ...</p>
+        ) : (
+          <p>Results : {dataToShow?.length}</p>
+        )}
+
+        <div className={styles.results}>
+          {dataToShow?.map((res, index) => {
+            return (
+              <div key={index + res.idMeal} className="searched-element">
+                <SearchedResult data={res} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default SearchBar;
