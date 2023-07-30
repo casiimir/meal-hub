ef7df78c4a2f42a29de2f587ec7b8ab551f4712e
Nignignina <ninetta.inchiappa@gmail.com>
2023-07-30 17:24:12 +0200

    feat:component

---
+++ src/pages/fridge/index.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import styles from "./fridge.module.scss";
+
+const Fridge = () => {
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, 400);
+  }, []);
+
+  return (
+    <div className={styles.Fridge}>
+      <div className={styles.wrapper}>
+        <div
+          className={`${styles.circle} ${
+            animate ? styles.animate : ""
+          } `}></div>{" "}
+      </div>
+    </div>
+  );
+};
+export default Fridge;
