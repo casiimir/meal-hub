00be09f5d282b2a0f69befbf18b01556548882cd
danielntll <dev@danielntll.it>
2023-08-02 12:56:35 +0200

    Fix: authcontext

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const auth = getAuth();
